aa505cd98258beac7ef83de1725f3998e7a3b735
komer <hyluo.1999@qq.com>
2025-12-19 17:25:22 +0800

    add nvim-orgmode

---
+++ dot_config/nvim/lua/plugins/org.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    "blink.cmp",
+    opts = {
+      sources = {
+        per_filetype = {
+          org = { "orgmode" },
+        },
+        providers = {
+          orgmode = {
+            name = "Orgmode",
+            module = "orgmode.org.autocompletion.blink",
+            fallbacks = { "buffer" },
+          },
+        },
+      },
+    },
+  },
+  {
+    "nvim-orgmode/org-bullets.nvim",
+    event = "VeryLazy",
+    ft = { "org" },
+    opts = {},
+  },
+  {
+    "nvim-orgmode/orgmode",
+    dependencies = {
+      "nvim-orgmode/org-bullets.nvim",
+      "danilshvalov/org-modern.nvim",
+    },
+    event = "VeryLazy",
+    ft = { "org" },
+    opts = {
+      ui = {
+        input = {
+          use_vim_ui = true,
+        },
+        menu = {
+          handler = function(data)
+            require("org-modern.menu")
+              :new({
+                window = {
+                  margin = { 1, 0, 1, 0 },
+                  padding = { 0, 1, 0, 1 },
+                  title_pos = "center",
+                  border = "single",
+                  zindex = 1000,
+                },
+                icons = {
+                  separator = "➜",
+                },
+              })
+              :open(data)
+          end,
+        },
+      },
+      org_agenda_files = "~/org/**/*",
+      org_default_notes_file = "~/org/refile.org",
+    },
+  },
+}
