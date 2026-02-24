a79e70b883b8f95134bc6c78bb1dc52940787d8e
hyluo <hyluo.1999@qq.com>
2026-02-24 21:13:38 +0800

    change doom emacs font to NF version

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Maple Mono CN" :size 15 :weight 'semi-light))
+(setq doom-font (font-spec :family "Maple Mono NF CN" :size 15 :weight 'semi-light))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
