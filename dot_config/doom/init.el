c33c705aefd4db59f230d6551eea4ef385f26d39
komer <hyluo.1999@qq.com>
2026-06-25 08:30:58 +0800

    pretty org mode

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -157,7 +157,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;; (ocaml +lsp)             ; an objective camel
-       (org +roam +crypt)               ; organize your plain life in plain text
+       (org +roam +crypt +pretty)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
